57024077575f104b3ecb8d6c080fc2db96b85c6a
gianvitodifilippo <gianvito.difilippo@gmail.com>
2025-11-09 23:33:25 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css'
 
 function App() {
+    return (
+        <div id="background">
+            <h1>Sito web in costruzione...</h1>
+            <h2>🎶🎸 Ci vediamo presto! 🎸🎶</h2>
+        </div>
+    );
 }
 
 export default App;
