0ed925b2b22d3a22b2da06a47a51df903d976111
gianvitodifilippo <gianvito.difilippo@gmail.com>
2026-08-26 14:43:46 +0200

    Update Annamaria's picture

--- src/lib/pgq/members.ts
+++ src/lib/pgq/members.ts
@@ -6,7 +6,7 @@ import vitoPhoto from "../../assets/images/vito-nicola-paradiso.jpg";
 
 export const members: Member[] = [
   { id: "vito", name: "Vito Nicola Paradiso", role: "Chitarra, composizione e direzione", photo: vitoPhoto, photoPosition: "top", portraitPosition: "20% center" },
-  { id: "annamaria", name: "Annamaria Plantamura", role: "Chitarra", photo: annamariaPhoto, portraitPosition: "32% center" },
+  { id: "annamaria", name: "Annamaria Plantamura", role: "Chitarra", photo: annamariaPhoto },
   { id: "gianvito", name: "Gianvito Difilippo", role: "Chitarra", photo: gianvitoPhoto, photoPosition: "top" },
   { id: "gabriella", name: "Gabriella Perniola", role: "Chitarra", photo: gabriellaPhoto },
   { id: "ernesto", name: "Ernesto Losavio", role: "Contrabbasso", photo: ernestoPhoto },
